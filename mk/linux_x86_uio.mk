8989061533095f11daee8df33c2bf556f8aec0ba
Dirk Hutter <hutter@compeng.uni-frankfurt.de>
2021-04-14 10:28:37 +0200

    Deactivated warnings in libtool build flow

--- mk/linux_x86_uio.mk
+++ mk/linux_x86_uio.mk
@@ -1,5 +1,5 @@
 CC=gcc
 INCLUDE=-I./include/ -I./src/ 
-CFLAGS=--std=gnu99 -Wall -Wunused-result -fno-tree-vectorize
+CFLAGS=--std=gnu99 -Wall -Wunused-result -Wno-format-truncation -fno-tree-vectorize
 LDFLAGS=-lpci -pthread
 CLEANUP_FILES = .lo .la .o '~' bin
